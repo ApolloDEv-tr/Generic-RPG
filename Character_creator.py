b16ca8d6a8d7b8c0527504af814655a32ca8f79c
Apollo <paridfaira@gmali.com>
2026-06-21 13:33:53 +0200

    first commit

---
+++ Character_creator.py
@@ -0,0 +1,72 @@
+name = input("Name: ")
+print('Choose your race:')
+print('1. Human')
+print('2. Elf')
+print('3. Dwarf')
+print('4. Angel')
+
+race_choice = input('Enter the number of your choice: ')
+
+if race_choice == '1':
+    print('You have chosen Human.', 'You have adrealine rush and can run faster than the others.')
+elif race_choice == '2':
+    print('You have chosen Elf.', 'You have a natural affinity for magic and can cast spells more effectively')
+elif race_choice == '3':
+    print('You have chosen Dwarf.', 'You can see in the dark and get more materials from mining.')
+else:
+    print('You have chosen Angel.', 'You can fly for a short period of time and can heal yourself and others.')
+
+print('Choose your class:')
+print('1. Warrior')
+print('2. Mage')
+print('3. Tank')
+print('4. Archer')
+print('5. Thief')
+
+class_choice = input('Enter the number of your choice: ')
+
+if class_choice == '1':
+    print('You have chosen Warrior.', 'You are specialized in meele combat.')
+    health = 100
+elif class_choice == '2':
+    print('You have chosen Mage.', 'You have big mana reserves and you are skilled in spell casting.')
+    health = 80
+elif class_choice == '3':
+    print('You have chosen  Tank', 'You have high health and high resistance to damage.')
+    health = 150
+elif class_choice == '4':
+    print('You have chosen Archer.', 'You are skilled in ranged combat and have high dexterity.')
+    health = 90
+else:
+    print('You have chosen Thief.', 'You are skilled in stealth and are more agile than otehr classes')
+    health = 70
+
+
+print('Choose your difficulty: ')
+print('1. Easy')
+print('2. Medium')
+print('3. Hard')
+print("4. Extreme")
+
+difficulty_choice = input('Enter the number of your choice: ')
+
+if difficulty_choice == '1':
+    health += 20
+    print('You have chosen Easy difficulty. Your health has been increased by 20 and enemies deal less damage.')
+elif difficulty_choice == '2':
+    print('You have chosen Medium difficulty. Your health remains the same and enemies deal normal damage.')
+elif difficulty_choice == '3':
+    health -= 20
+    print('You have chosen Hard difficulty. Your health has been decreased by 20.')
+else:
+    health -= 20
+    print('You have chosen Extreme difficulty. Your health has been decreased by 20 and enemies deal more damage.', 'Becareful, you have Permadeath.')
+
+player = {
+    'name': name,
+    'race': race_choice,
+    'class': class_choice,
+    'health': health,
+    'difficulty': difficulty_choice
+}
+print('Character created. Good Luck!')
